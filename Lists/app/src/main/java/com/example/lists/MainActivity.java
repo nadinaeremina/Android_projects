4868f019629d3e9dc6aca70b6b5a405035be3403
nadinaeremina <nadinaeremina@mail.ru>
2025-09-17 16:02:37 +0500

    ready

--- Lists/app/src/main/java/com/example/lists/MainActivity.java
+++ Lists/app/src/main/java/com/example/lists/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.lists;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] countries = {"USA", "RUSSIA", "CHINA", "TURKEY", "Japan", "Canada",
+            "USA", "RUSSIA", "CHINA", "TURKEY", "Japan", "Canada"};
+
+    ListView countriesList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,15 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        countriesList = findViewById(R.id.countriesList);
+
+        // создаем адаптер для нашего списка
+        // 1 - контекст (текущий), 2 - это готовый (встроенный layout), 3 - наш список
+        ArrayAdapter<String>adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, countries);
+
+        // устанавливаем наши страны для списка
+        countriesList.setAdapter(adapter);
     }
 }
